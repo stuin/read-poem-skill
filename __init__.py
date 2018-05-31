3b0f7983a10313ae7d568e31d5c0e4c007a74090
Stuart Irwin <stuin01@gmail.com>
2018-05-31 18:42:06 +0000

    Lots of work and attempts to test

--- __init__.py
+++ __init__.py
@@ -1,49 +1,53 @@
 # TODO: Add an appropriate license to your skill before publishing.  See
 # the LICENSE file for more information.
-
-# Below is the list of outside modules you'll be using in your skill.
-# They might be built-in to Python, from mycroft-core or from external
-# libraries.  If you use an external library, be sure to include it
-# in the requirements.txt file so the library is installed properly
-# when the skill gets installed later by a user.
-
 from adapt.intent import IntentBuilder
 from mycroft.skills.core import MycroftSkill, intent_handler
-from mycroft.util.log import LOG
+from mycroft.util.log import getLogger
 
 from os import walk
+from os.path import dirname
+__author__ = "Stuin"
 
-# Each skill is contained within its own class, which inherits base methods
-# from the MycroftSkill class.  You extend this class as shown below.
+LOGGER = getLogger(__name__)
 
-# TODO: Change "Template" to a unique name for your skill
 class ReadPoemSkill(MycroftSkill):
 
     # The constructor of the skill, which calls MycroftSkill's constructor
     def __init__(self):
         super(ReadPoemSkill, self).__init__(name="ReadPoemSkill")
-        
+
         # Initialize working variables used within the skill.
+    def make_list(self):
         f = []
         self.p = []
-        for (dirpath, dirnames, filenames) in walk(mypath):
+        path = dirname(__file__)+"/Poems"
+        for (dirpath, dirnames, filenames) in walk(path):
             f.extend(filenames)
             break
         for file in f:
-            self.p.add(file.remove(".txt").replace("-", " "));
+            file = file[:-4]
+            self.p.append(file.replace("-", " "))
 
+    def initialize(self):
+        self.load_data_files(dirname(__file__))
+        
+        intent = IntentBuilder("PoemIntent"). \
+            require("Recite").require("Poem").build()
+        self.register_intent(intent, self.handle_read_poem_intent)
 
-    @intent_handler(IntentBuilder("").require("Recite").op("Story").require("Poem"))
+    #@intent_handler(IntentBuilder("PoemIntent").require("Recite").require("Poem"))#.op("Story"))
     def handle_read_poem_intent(self, message):
+        self.speak_dialog("found")
         poem = message.data.get("Poem")
-        if p.contains(poem):
+
+        if self.p.contains(poem):
             self.file = poem.replace(" ", "-") + ".txt"
-            self.speak_dialog("found.here")
+            self.speak_dialog("found")
         else:
             self.speak_dialog("not.found")
 
     def stop(self):
-        return True
+        pass
 
 def create_skill():
-    return TemplateSkill()
+    return ReadPoemSkill()
